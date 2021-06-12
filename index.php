c012db6b1d07302e6941b9fdd2154dfc394d5d44
muhamadilhamh <ilham.habib190600@gmail.com>
2021-06-12 07:18:00 +0700

    update vitur

--- index.php
+++ index.php
@@ -13,157 +13,233 @@
 
 <body>
   <div id="App">
-    <title>Teknik Informatika</title>
-    <!-- Navbar -->
-    <v-toolbar dense>
-      <v-container>
-        <v-toolbar-title>
-          <v-card-action>
-            <v-btn href="index.php" class="btn" text> Mata <b>Kuliah</b></v-btn>
-          </v-card-action>
-          <v-card-action>
-            <v-btn href="about.html" class="btn" text>About Teknik Informatika</v-btn>
-          </v-card-action>
+    <v-app>
+      <div class="home">
+        <title>Teknik Informatika</title>
+        <!-- Navbar -->
+        <v-toolbar dense>
+          <v-container>
+            <v-toolbar-title>
+              <v-card-action>
+                <v-btn href="index.php" class="btn" text> Mata <b>Kuliah</b></v-btn>
+              </v-card-action>
+              <v-card-action>
+                <v-btn href="about.html" class="btn" text>About Teknik Informatika</v-btn>
+              </v-card-action>
 
-        </v-toolbar-title>
-      </v-container>
-    </v-toolbar>
+            </v-toolbar-title>
+          </v-container>
+        </v-toolbar>
 
-    <!-- Jumbotron-->
-    <section>
-      <div class="warna-bg">
-        <v-flex class="pa-10 pb-8 text-center">
-          <v-img class=" pict" src="./asset/tst4.svg" width="300"></v-img>
-        </v-flex>
-        <p class="title">Ada mata kuliah apa saja sih di Teknik Informatika</p>
-      </div>
-    </section>
-    <!-- Form Search -->
-    <v-container>
-      <v-row>
-        <v-col cols="12">
-          <form @submit.prevent="submit">
-
-            <v-text-field elevation="7" solo rounded tile label="Cari Data" color="primary" v-model="input" placeholder="Caro Data" @keypress.enter="show">
-              <template v-slot:append>
-
-                <v-btn class="btn" color="#90CAF9" type="submit" elevation="2">
-                  <v-icon>mdi-magnify</v-icon>
-                  Search
-                </v-btn>
-              </template>
-            </v-text-field>
-          </form>
-        </v-col>
-
-        <v-container>
-          <h2 v-model="input">Keyword yang dicari</h2>
-          <p> {{input}}</p>
-        </v-container>
-
-        <v-container>
-          <v-radio-group row @change="sortedItem = 'jenisMatkul'">
-            <v-radio label="Ascending" value="asc" @click="sortType = 'asc'"></v-radio>
-            <v-radio label="Descending" value="desc" @click="sortType = 'desc'"></v-radio>
-          </v-radio-group>
-
-        </v-container>
-
+        <!-- Jumbotron-->
+        <section>
+          <div class="warna-bg">
+            <v-flex class="pa-10 pb-1 text-center">
+              <v-img class=" pict" src="./asset/tst4.svg" width="280"></v-img>
+            </v-flex>
+            <p class="tema">Ada mata kuliah apa saja sih di Teknik Informatika</p>
+          </div>
+        </section>
+        <!-- Form Search -->
         <v-container>
           <v-row>
-            <v-col v-for="(item,index) in filteredList" :key="index" cols="12" md="4">
-              <v-col>
-                <v-card outlined class="mx-auto rounded-lg">
-                  <v-card-title>
-                    Nama Mata Kuliah:
-                  </v-card-title>
-                  <v-card-subtitle>
-                    <p>{{item.Matkul}}</p>
-                  </v-card-subtitle>
-                  <hr />
+            <v-col cols="12">
+              <form @submit.prevent="submit">
+                <v-text-field elevation="7" solo clearable rounded tile label="Cari Data" color="primary" v-model="input" placeholder="Cari Data" @keypress.enter="show">
+                  <template v-slot:append>
+                    <v-btn class="btn" color="#90CAF9" type="submit" elevation="2">
+                      <v-icon>mdi-magnify</v-icon>
+                      Search
+                    </v-btn>
+                  </template>
+                </v-text-field>
+              </form>
+            </v-col>
 
-                  <v-card-title>
-                    Jenis Mata Pelajaran :
-                  </v-card-title>
-                  <v-card-subtitle>
-                    <p>{{item.jenisMatkul}}</p>
-                  </v-card-subtitle>
-                  <v-card-title>
-                    Dosen Pengajar :
-                  </v-card-title>
-                  <v-card-subtitle>
-                    <p>{{item.dosenPengajar}}</p>
-                  </v-card-subtitle>
-                  <v-card-title>
-                    Jumlah Sks:
-                  </v-card-title>
-                  <v-card-subtitle>
-                    <p>{{item.jumlahSks}}</p>
-                  </v-card-subtitle>
-                  <v-card-title>
-                    Semester:
-                  </v-card-title>
-                  <v-card-subtitle>
-                    <p>{{item.semester}}</p>
-                  </v-card-subtitle>
+            <v-container>
+              <h2 v-model="input">Keyword yang dicari</h2>
+              <p> {{input}}</p>
+            </v-container>
 
-                  <v-card-actions>
 
-                    <template v-if="reveal != item.Matkul">
-                      <v-btn class="btn" color="primary" text @click="reveal = item.Matkul">
-                        <v-icon left>mdi-chevron-down</v-icon>
-                        Detail Mata Kuliah
-                      </v-btn>
-                    </template>
 
-                    <template v-if="reveal == item.Matkul">
-                      <v-btn class="btn" color="orange lighten-2" text @click="reveal = false">
-                        <v-icon left>mdi-chevron-up</v-icon>
-                        Back
-                      </v-btn>
-                    </template>
-                  </v-card-actions>
+            <v-container>
+              <v-row>
+                <v-col lg="2">
+                  <div class="clear">
+                    <v-btn outlined rounded solo @click="
+                showFilter =! showFilter;
+              ">
+                      <v-icon left> mdi-sort-variant </v-icon>
+                      Filter
+                    </v-btn>
+                  </div>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col lg="3" v-if="showFilter">
+                  <p class="filter">Jenis Matkul</p>
+                  <v-autocomplete clearable small-chips dense outlined rounded :items="jenisMatkul" v-model="selected_jenisMatkul" item-text="name" item-value="name" @change="getMatkul(selected_jenisMatkul)">
+                  </v-autocomplete>
+                </v-col>
+                <v-col lg="3" v-if="showFilter">
+                  <p class="filter">Semester</p>
+                  <v-autocomplete clearable small-chips dense outlined rounded :items="semester" v-model="selected_semester" item-text="name" item-value="name" @change="getMatkul(selected_semester)">
+                  </v-autocomplete>
+                </v-col>
+                <v-col lg="3" v-if="showFilter">
+                  <p class="filter">Jumlah sks</p>
+                  <v-autocomplete clearable small-chips dense outlined rounded :items="sks" v-model="selected_sks" item-text="name" item-value="name" @change="getMatkul(selected_sks)">
+                  </v-autocomplete>
+                </v-col>
+              </v-row>
+            </v-container>
 
-                  <v-expand-transition>
-                    <div v-if="reveal == item.Matkul">
+            <v-container>
+              <v-row>
+                <v-col v-for=" (item,index) in filteredList" :key="index" cols="12" md="4">
+                  <v-col>
+                    <v-card outlined class="mx-auto rounded-lg">
+                      <v-card-title>
+                        Nama Mata Kuliah:
+                      </v-card-title>
+                      <v-card-subtitle>
+                        <h2>{{item.Matkul}}</h2>
+                      </v-card-subtitle>
+                      <hr>
+
+                      <v-card-title>
+                        Jenis Mata Pelajaran :
+                      </v-card-title>
+                      <v-card-subtitle>
+                        <h4 class="bg_jenisMatkul">{{item.jenisMatkul}}</h4>
+                      </v-card-subtitle>
                       <v-divider></v-divider>
                       <v-card-title>
-                        Deskripsi
+                        Dosen Pengajar :
                       </v-card-title>
-                      <v-card-text>
-                        {{item.deskripsi}}
-                      </v-card-text>
+                      <v-card-subtitle>
+                        <h4 class="bg_dosen">{{item.dosenPengajar}}</h4>
+                      </v-card-subtitle>
                       <v-divider></v-divider>
-                    </div>
-                  </v-expand-transition>
-                </v-card>
-              </v-col>
+                      <v-card-title>
+                        Jumlah Sks:
+                      </v-card-title>
+                      <v-card-subtitle>
+                        <h4 class="bg_sks">{{item.jumlahSks}}</h4>
+                      </v-card-subtitle>
+                      <v-divider></v-divider>
+                      <v-card-title>
+                        Semester:
+                      </v-card-title>
+                      <v-card-subtitle>
+                        <h4 class="bg_semester">{{item.semester}}</h4>
+                      </v-card-subtitle>
+
+                      <v-card-actions>
+
+                        <template v-if="reveal != item.Matkul">
+                          <v-btn class="btn" color="primary" text @click="reveal = item.Matkul">
+                            <v-icon left>mdi-chevron-down</v-icon>
+                            Detail Mata Kuliah
+                          </v-btn>
+                        </template>
+
+                        <template v-if="reveal == item.Matkul">
+                          <v-btn class="btn" color="primary" text @click="reveal = false">
+                            <v-icon left>mdi-chevron-up</v-icon>
+                            Back
+                          </v-btn>
+                        </template>
+                      </v-card-actions>
+
+                      <v-expand-transition>
+                        <div v-if="reveal == item.Matkul">
+                          <v-divider></v-divider>
+                          <v-card-title>
+                            Deskripsi
+                          </v-card-title>
+                          <v-card-text>
+                            {{item.deskripsi}}
+                          </v-card-text>
+                          <v-divider></v-divider>
+                        </div>
+                      </v-expand-transition>
+                    </v-card>
+                  </v-col>
+                </v-col>
+              </v-row>
+            </v-container>
+            <v-btn class="load_more" text large v-if="loadList == true" @click="listToShow += 6" block>
+              Load More
+            </v-btn>
             </v-col>
           </v-row>
         </v-container>
-        </v-col>
-      </v-row>
-
-    </v-container>
-
+      </div>
+    </v-app>
   </div>
   <footer>&copy; MataKuliah</footer>
+
+
 
 </body>
 <style>
   .pict {
     left: 40%;
     right: 40%;
-
   }
 
-  .title {
-    font-size: 2rem;
+  .filter {
+    font-size: 1.2rem;
+    font-weight: 500;
+    color: rgb(163, 143, 143);
+  }
+
+
+  .tema {
+    font-size: 30px;
+    text-align: center;
     color: white;
     font-weight: 700;
-    margin-left: 50px;
+    margin-bottom: 100px;
+  }
+
+  .home {
+    font-family: Poppins;
+    font-weight: 400;
+  }
+
+  .bg_sks {
+    background-color: rgb(0, 110, 255);
+    border-radius: 10px;
+    width: 50px;
     text-align: center;
-    margin-bottom: 50px;
+    color: white;
+  }
+
+  .bg_semester {
+    background-color: rgb(0, 110, 255);
+    border-radius: 10px;
+    width: 90px;
+    text-align: center;
+    color: white;
+  }
+
+  .bg_dosen {
+    background-color: rgb(0, 110, 255);
+    border-radius: 10px;
+    width: 250px;
+    text-align: center;
+    color: white;
+  }
+
+  .bg_jenisMatkul {
+    background-color: rgb(0, 110, 255);
+    border-radius: 10px;
+    width: auto;
+    text-align: center;
+    color: white;
   }
 
   .btn {
@@ -181,40 +257,96 @@
       reveal: [{
 
       }],
-      listToShow: 12,
+      showFilter: false,
+      selected_sks: '',
+      sks: [{
+          name: "1 sks",
+        },
+        {
+          name: "2 sks",
+        },
+        {
+          name: "3 sks",
+        },
+        {
+          name: "7 sks"
+        },
+
+      ],
+      selected_semester: '',
+      semester: [{
+          name: "Semester 1",
+        },
+        {
+          name: "Semester 2",
+        },
+        {
+          name: "Semester 3",
+        },
+        {
+          name: "Semester 4",
+        },
+        {
+          name: "Semester 5",
+        },
+        {
+          name: "Semester 6",
+        },
+        {
+          name: "Semester 7",
+        },
+        {
+          name: "Semester 8",
+        },
+      ],
+      selected_jenisMatkul: '',
+      jenisMatkul: [{
+          name: "Wajib"
+        },
+        {
+          name: "Peminatan"
+        },
+
+      ],
+      listToShow: 6,
+      loadList: false,
       input: '',
       matkul_data: [],
       query: '',
-      sortedItem: '',
       sortType: '',
-      nodata: false
+      sortedItem: '',
+      list: [],
     },
     methods: {
+      getMatkul(value) {
+        axios.post('proses.php', {
+          data: value
+        }).then(response => {
+          this.matkul_data = response.data
+        })
+
+      },
       submit() {
         axios.post('proses.php', {
           data: this.input
         }).then(response => {
           this.matkul_data = response.data
         })
-      }
+      },
     },
+
     created() {
       axios.get('proses.php', {
         data: this.input
       }).then(response => {
+        this.loadList = true
         this.matkul_data = response.data
       })
+
     },
     computed: {
       filteredList: function() {
-        return Object.values(this.matkul_data).sort((p1, p2) => {
-          if (this.sortedItem != 'Matkul' || this.sortedItem != 'jenisMatkul') {
-            let modifier = 1;
-            if (p1[this.sortedItem] < p2[this.sortedItem]) return -1 * modifier;
-            if (p1[this.sortedItem] > p2[this.sortedItem]) return 1 * modifier;
-            return 0;
-          } else {}
-        })
+        return Object.values(this.matkul_data).slice(0, this.listToShow)
       }
     }
   });
